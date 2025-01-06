6a8bcee77634753c24d67abd1ef0b72d40482b5a
Luis <81855420+luisoos@users.noreply.github.com>
2025-01-06 12:20:38 +0100

    💄 chng: minor updates
    will need real changes in the future

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
     <div class="projects section-below">
       <h2 class="text-xl font-bold sm:text-lg md:text-2xl text-left font-font-extrabold"> What do I do? </h2>
       <p class="text-left"> <span class="opacity-80"> 
-        I am working on multiple things. Currently, I am working on CI/CD in projects, but I am also 
+        I am working on multiple things. Currently, I am working on fullstack projects with companies, but I am also 
         building some things on the side that got me interested. Open-source projects can be found on my </span>
         <a href="https://github.com/luisoos" target="_blank" rel="noreferrer">GitHub</a><span class="opacity-80">.</span>
       </p>
@@ -62,15 +62,9 @@
       <div class="technologies section-below">
         <h2 class="text-xl font-bold sm:text-lg md:text-2xl text-left font-font-extrabold">Technologies</h2>
         <p class="opacity-80 text-left"> I use many different technologies to build the best matching tech stacks for projects. However, 
-          I do most things in JavaScript/TypeScript. Below is a list of my most-used technologies. </p>
+          these are my most-used and prefered technologies. </p>
         <div class="techstack">
           <ul class="grid gap-4 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-            <li class="flex space-x-2">
-              <span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><path fill="#ffffff" d="M2 1v125h125V1H2zm66.119 106.513c-1.845 3.749-5.367 6.212-9.448 7.401-6.271 1.44-12.269.619-16.731-2.059-2.986-1.832-5.318-4.652-6.901-7.901l9.52-5.83c.083.035.333.487.667 1.071 1.214 2.034 2.261 3.474 4.319 4.485 2.022.69 6.461 1.131 8.175-2.427 1.047-1.81.714-7.628.714-14.065C58.433 78.073 58.48 68 58.48 58h11.709c0 11 .06 21.418 0 32.152.025 6.58.596 12.446-2.07 17.361zm48.574-3.308c-4.07 13.922-26.762 14.374-35.83 5.176-1.916-2.165-3.117-3.296-4.26-5.795 4.819-2.772 4.819-2.772 9.508-5.485 2.547 3.915 4.902 6.068 9.139 6.949 5.748.702 11.531-1.273 10.234-7.378-1.333-4.986-11.77-6.199-18.873-11.531-7.211-4.843-8.901-16.611-2.975-23.335 1.975-2.487 5.343-4.343 8.877-5.235l3.688-.477c7.081-.143 11.507 1.727 14.756 5.355.904.916 1.642 1.904 3.022 4.045-3.772 2.404-3.76 2.381-9.163 5.879-1.154-2.486-3.069-4.046-5.093-4.724-3.142-.952-7.104.083-7.926 3.403-.285 1.023-.226 1.975.227 3.665 1.273 2.903 5.545 4.165 9.377 5.926 11.031 4.474 14.756 9.271 15.672 14.981.882 4.916-.213 8.105-.38 8.581z"/></svg>
-              </span>
-              <span class="tech-text">JavaScript</span>
-            </li>
             <li class="flex space-x-2">
               <span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><path fill="#ffffff" d="M2 63.91v62.5h125v-125H2zm100.73-5a15.56 15.56 0 017.82 4.5 20.58 20.58 0 013 4c0 .16-5.4 3.81-8.69 5.85-.12.08-.6-.44-1.13-1.23a7.09 7.09 0 00-5.87-3.53c-3.79-.26-6.23 1.73-6.21 5a4.58 4.58 0 00.54 2.34c.83 1.73 2.38 2.76 7.24 4.86 8.95 3.85 12.78 6.39 15.16 10 2.66 4 3.25 10.46 1.45 15.24-2 5.2-6.9 8.73-13.83 9.9a38.32 38.32 0 01-9.52-.1A23 23 0 0180 109.19c-1.15-1.27-3.39-4.58-3.25-4.82a9.34 9.34 0 011.15-.73l4.6-2.64 3.59-2.08.75 1.11a16.78 16.78 0 004.74 4.54c4 2.1 9.46 1.81 12.16-.62a5.43 5.43 0 00.69-6.92c-1-1.39-3-2.56-8.59-5-6.45-2.78-9.23-4.5-11.77-7.24a16.48 16.48 0 01-3.43-6.25 25 25 0 01-.22-8c1.33-6.23 6-10.58 12.82-11.87a31.66 31.66 0 019.49.26zm-29.34 5.24v5.12H57.16v46.23H45.65V69.26H29.38v-5a49.19 49.19 0 01.14-5.16c.06-.08 10-.12 22-.1h21.81z"/></svg>
